a860b0ff05cb7b1edc4f20384b3d8d7b3ea3569a
fabiencaseiropro-ux <fabiencaseiropro@gmail.com>
2026-06-23 13:35:31 +0200

    Fix sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,21 @@
-const CACHE = 'fyl-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const CACHE = 'fyl-v2';
+const BASE = '/fyl-fit';
+const FILES = [
+  BASE + '/',
+  BASE + '/index.html',
+  BASE + '/manifest.json',
+  BASE + '/icon-192.png',
+  BASE + '/icon-512.png',
+];
 
 self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()))
 );
 
 self.addEventListener('activate', e =>
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ))
+  ).then(() => self.clients.claim()))
 );
 
 self.addEventListener('fetch', e =>
